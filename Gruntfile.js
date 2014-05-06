dd88272ad2b8c6ae9455ef0d13185f5c55ee989e
Alex Slubsky <aslubsky@gmail.com>
2014-05-06 08:23:27 +0300

    init

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,61 @@
 module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('dev', [
+        'less',
+        'copy',
+        'php:dev',
+        'watch'
+    ]);
+    grunt.registerTask('default', [
+        'ngTemplateCache',
+        'less',
+        'copy',
+        'requirejs',
+        'uglify',
+        'htmlmin',
+        'cssmin',
+        'replace'
+    ]);
+
+    require('./build-config.js');
+
+    var theme = grunt.option('theme') || 'default',
+        themeCfg = grunt.file.readJSON('./themes/' + theme + '/config.json');
+
+    themeCfg.css = themeCfg.css || {};
+//    console.info(themeCfg);
+
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cfg: bazalt,
+        theme: themeCfg,
+        theme_dir: './themes/' + theme,
+        build_dir: 'build',
+        banner: 'Developed by EqualTeam (http://equalteam.net/)',
         copy: {
-            theme: {
+            api: {
                 files: [
                     {
-                        expand: true,
                         src: [
-                            'app/menu.json',
-                            'themes/default/assets/**',
-                            '!themes/default/assets/less/**',
-                            'favicon.png'
+                            'api/**'
                         ],
                         dest: 'build/'
                     }
                 ]
             },
-            socketio: {
+            theme: {
                 files: [
                     {
                         expand: true,
                         src: [
-                            'app/vendors/socket.io.min.js'
+                            'app/*.json',
+                            'locale/*',
+                            'themes/<%= cfg.theme %>/assets/**',
+                            '!themes/<%= cfg.theme %>/assets/less/**',
+                            'favicon.png'
                         ],
-                        rename: function(dest, src) {
-                            return dest + 'socket-io.js';
-                        },
                         dest: 'build/'
                     }
                 ]
@@ -33,11 +64,28 @@ module.exports = function (grunt) {
         less: {
             bootstrap: {
                 src: ['bower_components/bootstrap/less/bootstrap.less'],
-               dest: 'themes/default/assets/css/bootstrap.css'
+                dest: 'themes/<%= cfg.theme %>/assets/css/bootstrap.css'
             },
             theme: {
-                src: ['themes/default/assets/less/theme.less'],
-                dest: 'themes/default/assets/css/theme.css'
+                files: {'themes/<%= cfg.theme %>/assets/css/theme.css': 'themes/<%= cfg.theme %>/assets/less/theme.less'},
+                options: {
+                    cleancss: true,
+                    modifyVars: themeCfg.css
+                }
+            }
+        },
+        cssmin: {
+            theme: {
+                files: {
+                    '<%= build_dir %>/themes/<%= cfg.theme %>/assets/css/theme.css': [
+                        '<%= build_dir %>/themes/<%= cfg.theme %>/assets/css/bootstrap.css',
+                        '<%= build_dir %>/themes/<%= cfg.theme %>/assets/css/theme.css'
+                    ]
+                }
+            },
+            options: {
+                banner: '/*! <%= banner %> */',
+                keepSpecialComments: '0'
             }
         },
         requirejs: {
@@ -49,64 +97,52 @@ module.exports = function (grunt) {
                     useStrict: true,
                     wrap: {
                         start: "(function() {",
-                        end: "require(['main']) }());"
+                        end: "requirejs.config({" +
+                            "urlArgs: '_=<%= new Date().getTime() %>'"
+                            +
+                            "});" +
+                            " }());"
                     },
-                    mainConfigFile: './app/config.js',
+                    mainConfigFile: './app/appBootstrap.js',
                     name: 'main',
                     include: [],
-                    exclude: ['./views.js', './vendors/socket.io.min.js'],
-                    out: 'build/js/main.min.js'
+                    exclude: ['./views.js'],
+                    out: '<%= build_dir %>/main.js'
                 }
             }
         },
         uglify: {
+            requirejs: {
+                src: ['bower_components/requirejs/require.js'],
+                dest: '<%= build_dir %>/r.js'
+            },
             frontend: {
-                src: ['build/js/main.min.js'],
-                dest: 'build/js/main.min.js'
+                src: ['<%= build_dir %>/main.js'],
+                dest: '<%= build_dir %>/main.js'
             },
             options: {
+                banner: '/*! <%= banner %> */\n',
                 compress: false,
                 mangle: false,
                 preserveComments: false,
                 beautify: {
                     ascii_only: true
-                },
-                sourceMappingURL: function (fileName) {
-                    return fileName.replace(/^build\/js\//, '').replace(/\.js$/, '.map');
-                },
-                sourceMap: function (fileName) {
-                    return fileName.replace(/\.js$/, '.map');
                 }
             }
         },
         watch: {
             css: {
-                files: 'themes/**/assets/**/*.less',
+                files: 'themes/<%= cfg.theme %>/assets/**/*.less',
                 tasks: ['less'],
                 options: {
                     livereload: true
-                }
-            }//,
-//            html: {
-//                files: ['themes/**/views/**/*.html'],
-//                tasks: ['i18nextract'],
-//                options: {
-//                    livereload: true
-//                }
-//            }
-        },
-        php: {
-            dev: {
-                options: {
-                    hostname: '127.0.0.1',
-                    port: 8081
                 }
             }
         },
         htmlmin: {
             backend: {
                 files: {
-                    'build/index.html': 'themes/default/index.html'
+                    '<%= build_dir %>/index.html': 'themes/<%= cfg.theme %>/index.html'
                 },
                 options: {
                     removeComments: true,
@@ -117,25 +153,40 @@ module.exports = function (grunt) {
                 }
             }
         },
+        php: {
+            dev: {
+                options: {
+                    hostname: '127.0.0.1',
+                    port: 8080
+                }
+            }
+        },
         replace: {
             default: {
-                src: 'build/index.html',
+                src: '<%= build_dir %>/index.html',
                 overwrite: true,
                 replacements: [
                     {
                         from: /<script src="(.*)\/require.js"(.*)><\/script>/gm,
-                        to: '<script src="/js/main.min.js"></script>'//<script src="/js/app/vendors/socket.io.min.js"></script>
+                        to: '<script src="/r.js" data-main="main"></script>'
+                    },
+                    {
+                        from: /theme.css/gm,
+                        to: 'theme.css?_=<%= new Date().getTime() %>'
+                    },
+                    {
+                        from: '<script src="/build-config.js"></script>',
+                        to: ''
+                    },
+                    {
+                        from: '<link href="/themes/default/assets/css/bootstrap.css" rel="stylesheet" media="screen">',
+                        to: ''
+                    },
+                    {
+                        from: '<!DOCTYPE html>',
+                        to: '<!DOCTYPE html>\n<!-- <%= banner %> -->\n'
                     }
                 ]
-            }
-        },
-        i18nextract: {
-            backend: {
-                lang: ['uk_UA', 'ru_RU'],
-                src: ['themes/*/views/**/*.html', 'index.html'],
-                dest: 'locale',
-                prefix: 'theme-',
-                safeMode: false
             }
         },
         ngTemplateCache: {
@@ -150,29 +201,4 @@ module.exports = function (grunt) {
             }
         }
     });
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-text-replace');
-    grunt.loadNpmTasks('grunt-angular-translate');
-    grunt.loadNpmTasks('grunt-php');
-    grunt.loadNpmTasks('grunt-hustler');
-
-    grunt.registerTask('dev', [
-        'copy',
-        'php:dev',
-        'watch'
-    ]);
-    grunt.registerTask('default', [
-        'ngTemplateCache',
-        'less:theme',
-        'copy',
-        'requirejs',
-        'uglify',
-        'htmlmin',
-        'replace'
-    ]);
 };
---
+++ build-config.js
@@ -0,0 +1,11 @@
+bazalt = {
+    api: '/api/rest.php',
+    errorTemplates: {
+        403: '/themes/default/views/error/403.html',
+        404: '/themes/default/views/error/404.html'
+    },
+    checkSessionOnStart: false,
+    languages: ['uk_UA', 'ru_RU'],
+    language: 'uk_UA',
+    theme: 'default'
+};
